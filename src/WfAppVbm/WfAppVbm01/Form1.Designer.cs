0cccb31883d49b9cbc4ad7cb48a2cf0bbc8439f0
Nhoppasit S <nhoppasit@gmail.com>
2024-05-25 21:10:05 +0700

    Add code for quite mode install sql express. << OK >> ไม่สามารถแสดงการโต้ตอบที่หน้าจอได้ ไม่ดี ต้องปรับปรุง

--- src/WfAppVbm/WfAppVbm01/Form1.Designer.cs
+++ src/WfAppVbm/WfAppVbm01/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WfAppVbm01
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,33 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnInstallSqlExpress = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnInstallSqlExpress
             // 
-            this.button1.Location = new System.Drawing.Point(31, 21);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnInstallSqlExpress.Location = new System.Drawing.Point(12, 12);
+            this.btnInstallSqlExpress.Name = "btnInstallSqlExpress";
+            this.btnInstallSqlExpress.Size = new System.Drawing.Size(114, 86);
+            this.btnInstallSqlExpress.TabIndex = 0;
+            this.btnInstallSqlExpress.Text = "Install SQL Express 2019";
+            this.btnInstallSqlExpress.UseVisualStyleBackColor = true;
+            this.btnInstallSqlExpress.Click += new System.EventHandler(this.btnInstallSqlExpress_Click);
             // 
-            // Form1
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.btnInstallSqlExpress);
+            this.Name = "frmMain";
+            this.Text = "VBM 2024";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnInstallSqlExpress;
     }
 }
 
--- src/WfAppVbm/WfAppVbm01/Form1.cs
+++ src/WfAppVbm/WfAppVbm01/Form1.cs
@@ -1,20 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
 
 namespace WfAppVbm01
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void btnInstallSqlExpress_Click(object sender, EventArgs e)
+        {
+            string installerPath = Path.Combine(Application.StartupPath, "Resources\\SQL2019-SSEI-Expr.exe");
+
+            if (!File.Exists(installerPath))
+            {
+                MessageBox.Show("Installer file not found!");
+                return;
+            }
+
+            // Start the installer process
+            Process installer = new Process();
+            installer.StartInfo.FileName = installerPath;
+            installer.StartInfo.Arguments = "/Q"; // Quiet mode (no user interaction)
+            installer.StartInfo.UseShellExecute = false;
+            installer.StartInfo.RedirectStandardOutput = true;
+            installer.StartInfo.RedirectStandardError = true;
+
+            installer.OutputDataReceived += (s, ea) => AppendOutput(ea.Data);
+            installer.ErrorDataReceived += (s, ea) => AppendOutput(ea.Data);
+
+            installer.Start();
+            installer.BeginOutputReadLine();
+            installer.BeginErrorReadLine();
+
+            installer.WaitForExit();
+
+            MessageBox.Show("Installation process completed.");
+        }
+
+        private void AppendOutput(string data)
+        {
+            if (!string.IsNullOrEmpty(data))
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    System.Diagnostics.Trace.WriteLine(data);
+                    //txtOutput.AppendText(data + Environment.NewLine);
+                });
+            }
         }
     }
 }
