4ccbefa37a352b800ff1df4fb3251bdb3d0bc7df
Nhoppasit S <nhoppasit@gmail.com>
2024-05-26 21:56:05 +0700

    Use threshold in random select << OK >>

--- db/Lab0/exe random select of readings.sql
+++ db/Lab0/exe random select of readings.sql
@@ -5,7 +5,8 @@ GO -- DECLARE @return_value int EXEC @return_value = [dbo].[get_5_acceleration_r
 	-- 	@end_datetime = '2024-05-26 08:10:00'
 	-- SELECT 'Return Value' = @return_value
 	-- GO
-	EXEC [dbo].[get_5_acceleration_readings_between_timestamp] @measurement_id = 1,
+	EXEC [dbo].[get_5_acceleration_readings_between_timestamp] 
+	@measurement_id = 1,
 	@start_datetime = '2024-05-26 08:00:00',
 	@end_datetime = '2024-05-26 08:10:00',
-	@num_rows = 50;
+	@random_threshold = 0.8;
--- db/Lab0/get_5_acceleration_readings_between_timestamp.sql
+++ db/Lab0/get_5_acceleration_readings_between_timestamp.sql
@@ -1,24 +1,21 @@
-ALTER PROCEDURE [dbo].[get_5_acceleration_readings_between_timestamp] (
-    @measurement_id INT,
-    @start_datetime DATETIME,
-    @end_datetime DATETIME,
-    @num_rows INT
-) AS BEGIN
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO ALTER PROCEDURE [dbo].[get_5_acceleration_readings_between_timestamp] (
+        @measurement_id INT,
+        @start_datetime DATETIME,
+        @end_datetime DATETIME,
+        @random_threshold FLOAT
+    ) AS BEGIN
 SET NOCOUNT ON;
-PRINT @measurement_id PRINT @start_datetime PRINT @end_datetime PRINT @num_rows;
-WITH RandomizedReadings AS (
-    SELECT *,
-        ROW_NUMBER() OVER (
-            ORDER BY NEWID()
-        ) AS RowNum
-    FROM acceleration_readings
-    WHERE CAST(
-            CHECKSUM(NEWID(), reading_id) & 0x7fffffff AS float
-        ) / CAST (0x7fffffff AS int) < 0.8
-        AND measurement_id = @measurement_id
-        AND [timestamp] BETWEEN @start_datetime AND @end_datetime
-)
-SELECT TOP (@num_rows) *
-FROM RandomizedReadings;
+PRINT @measurement_id PRINT @start_datetime PRINT @end_datetime PRINT @random_threshold
+SELECT *
+FROM acceleration_readings TABLESAMPLE (5000 ROWS)
+WHERE @random_threshold >= CAST(
+        CHECKSUM(NEWID(), reading_id) & 0x7fffffff AS float
+    ) / CAST (0x7fffffff AS int)
+    AND measurement_id = @measurement_id
+    AND [timestamp] BETWEEN @start_datetime AND @end_datetime;
+PRINT @@ROWCOUNT
 END
 GO
