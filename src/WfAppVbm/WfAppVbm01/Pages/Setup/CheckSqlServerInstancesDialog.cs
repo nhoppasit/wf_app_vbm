0567a3ac32f334be412f1c5b010b92673fed1674
Nhoppasit S <nhoppasit@gmail.com>
2024-06-05 23:31:49 +0700

    something wrong! i want to check selection at least. if there is no "yes" in the column we will ask the confirmation <<OK>>

--- src/WfAppVbm/WfAppVbm01/Pages/Setup/CheckSqlServerInstancesDialog.cs
+++ src/WfAppVbm/WfAppVbm01/Pages/Setup/CheckSqlServerInstancesDialog.cs
@@ -3,6 +3,7 @@ using System;
 using System.Data;
 using System.Data.Sql;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,15 +16,28 @@ namespace WfAppVbm01.Pages.Setup {
 
             ListViewDatabaseInstances.FullRowSelect = true;
             ListViewDatabaseInstances.GridLines = true;
+            ListViewDatabaseInstances.MultiSelect = false;
 
             ListViewDatabaseInstances.ItemSelectionChanged += ListViewDatabaseInstances_ItemSelectionChanged;
 
             busyIndicator = new BusyIndicator();
         }
 
+        protected override void OnFormClosing(FormClosingEventArgs e) {
+            base.OnFormClosing(e);
+            bool anyItemSelected = ListViewDatabaseInstances.SelectedItems.Count > 0;
+            bool anySelectedHasDefault = ListViewDatabaseInstances.SelectedItems.Cast<ListViewItem>().Any(item => item.SubItems[4].Text == "Yes");
+            if (!anyItemSelected || !anySelectedHasDefault) {
+                var result = MessageBox.Show("You are about to quit without selecting a default SQL Server instance. Are you sure you want to quit?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.No) {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        readonly int defaultColumnIndex = 4;
         private void ListViewDatabaseInstances_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e) {
             if (e.IsSelected) {
-                var defaultColumnIndex = 4;
                 foreach (ListViewItem item in ListViewDatabaseInstances.Items) {
                     item.SubItems[defaultColumnIndex].Text = "";
                 }
@@ -33,9 +47,11 @@ namespace WfAppVbm01.Pages.Setup {
         }
 
         private async void DiscoveringDatabaseInstancesButton_Click(object sender, EventArgs e) {
+            DiscoveringDatabaseInstancesButton.Enabled = false;
             this.busyIndicator.Show(DiscoveringDatabaseInstancesButton);
             await Task.Run(() => DiscoveringDatabaseInstances());
             this.busyIndicator.Hide();
+            DiscoveringDatabaseInstancesButton.Enabled = true;
         }
 
         void DiscoveringDatabaseInstances() {
@@ -50,7 +66,7 @@ namespace WfAppVbm01.Pages.Setup {
                 }
                 this.Invoke((MethodInvoker)delegate {
                     int rowNo = 1;
-                    this.listView.Items.Clear();
+                    this.ListViewDatabaseInstances.Items.Clear();
                     foreach (DataRow row in dataTable.Rows) {
                         string serverName = row["ServerName"].ToString();
                         string instanceName = row["InstanceName"].ToString();
@@ -59,11 +75,13 @@ namespace WfAppVbm01.Pages.Setup {
                         Trace.WriteLine($"SQL Server instance found: {serverName}\\{instanceName} (Version: {version})");
 
                         // Add the instance information to the ListView
-                        ListViewItem item = new ListViewItem(serverName);
+                        ListViewItem item = new ListViewItem(rowNo.ToString());
+                        item.SubItems.Add(serverName);
                         item.SubItems.Add(instanceName);
                         item.SubItems.Add(version);
                         item.SubItems.Add(""); // Initialize Default column as empty
                         ListViewDatabaseInstances.Items.Add(item);
+                        rowNo++;
                     }
                 });
             } catch (Exception ex) {
@@ -74,5 +92,4 @@ namespace WfAppVbm01.Pages.Setup {
             }
         }
     }
-}
 }
