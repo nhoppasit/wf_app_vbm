e40bca5786b10b5b747060a3836420fc3b3c972e
Nhoppasit S <nhoppasit@gmail.com>
2024-06-09 20:13:50 +0700

    Try to complete AddNew measurement info

--- src/WfAppVbm/AccelerometerDatabase/MeasurementInfo.cs
+++ src/WfAppVbm/AccelerometerDatabase/MeasurementInfo.cs
@@ -18,21 +18,18 @@ namespace AccelerometerDatabase {
             try {
                 using (GenericManagement_MSSQL db = new GenericManagement_MSSQL()) {
                     db.ConnectionString = _connectionString;
-                    Dictionary<string, ParameterStructure_MSSQL> Inputs = new Dictionary<string, ParameterStructure_MSSQL>(){
-
-                        { "@val_parametername", new ParameterStructure_MSSQL("@val_parametername", SqlDbType.NVarChar, measurementInfo.??) },
-
+                    Dictionary<string, ParameterStructure_MSSQL> Inputs = new Dictionary<string, ParameterStructure_MSSQL>() {
+                        { "@first_start_time", new ParameterStructure_MSSQL("@first_start_time", SqlDbType.DateTime, measurementInfo.FirstStartTime) },
+                        { "@sensor_description", new ParameterStructure_MSSQL("@sensor_description", SqlDbType.NVarChar, measurementInfo.SensorDescription) },
+                        { "@serial_port", new ParameterStructure_MSSQL("@serial_port", SqlDbType.NVarChar, measurementInfo.SerialPort) }
                     };
-
-                    Dictionary<string, ParameterStructure_MSSQL> Output = new Dictionary<string, ParameterStructure_MSSQL>()
-                    {
-                        { "@RowCount", new ParameterStructure_MSSQL("@RowCount", SqlDbType.Int)},
-                        { "@MessageResult", new ParameterStructure_MSSQL("@MessageResult", SqlDbType.NVarChar, null, 200) },
-                         { "@CodeValue", new ParameterStructure_MSSQL("@CodeValue", SqlDbType.Int) }
+                    Dictionary<string, ParameterStructure_MSSQL> Output = new Dictionary<string, ParameterStructure_MSSQL>() {
+                        { "@RowCount", new ParameterStructure_MSSQL("@RowCount", SqlDbType.Int) },
+                        { "@MessageResult", new ParameterStructure_MSSQL("@MessageResult", SqlDbType.NVarChar, null, 1000) },
+                        { "@CodeValue", new ParameterStructure_MSSQL("@CodeValue", SqlDbType.Int) }
                     };
-
                     ResultTypeDS result = new ResultTypeDS();
-                    result.DataSetResult = db.ExecuteToDataSet("sp_Pet_AddNew", Inputs, ref Output);
+                    result.DataSetResult = db.ExecuteToDataSet("sp_measurement_info_addnew", Inputs, ref Output);
                     int rowCount = Convert.ToInt32(Output["@RowCount"].dbValue);
                     result.Code = Convert.ToInt32(Output["@CodeValue"].dbValue);
                     result.RowCount = rowCount;
@@ -45,5 +42,4 @@ namespace AccelerometerDatabase {
             }
         }
     }
-
 }
